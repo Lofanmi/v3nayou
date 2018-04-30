6d15cae45a4d4cadaa9fb85e44419a2973dd8200
linhaotong <852226732@qq.com>
2018-04-30 20:50:35 +0800

    sysu icons

--- routes/start.go
+++ routes/start.go
@@ -21,14 +21,14 @@ var (
 			map[string]string{"name": "课表", "link": "schedule", "icon": "/static/icons/schedule.png"},
 			map[string]string{"name": "校历", "link": "http://mp.weixin.qq.com/s/WIn3rG1BjxRx3EdOn9zvrw", "icon": "/static/icons/calendar.png"},
 			map[string]string{"name": "个人中心", "link": "ucenter", "icon": "/static/icons/ucenter.png"},
-			map[string]string{"name": "四六级", "link": "[empty]", "icon": "/static/icons/cet.png"},
-			map[string]string{"name": "时间胶囊", "link": "[empty]", "icon": "/static/icons/capsule.png"},
+			map[string]string{"name": "四六级", "link": "http://m.tool.finded.net/index.php?m=Cet&school=gzhu", "icon": "/static/icons/cet.png"},
+			map[string]string{"name": "实时公交", "link": "http://m.tool.finded.net/index.php?m=Bus&school=gzhu", "icon": "/static/icons/bus.png"},
 			map[string]string{"name": "教学评价", "link": "[empty]", "icon": "/static/icons/teacher.png"},
 			map[string]string{"name": "图书", "link": "http://m.book.finded.net/?code=1-3xwYVV6uCCyWxeNU-1xBtPHA-2x-2x", "icon": "/static/icons/books.png"},
 		},
 		"gzhu.icons.others": []map[string]string{
-			map[string]string{"name": "公选排行榜", "link": "[empty]", "icon": "/static/icons/charts.png"},
-			map[string]string{"name": "常用电话", "link": "http://m.tool.finded.net/index.php?m=Baike&c=Contacts&a=index", "icon": "/static/icons/strategy.png"},
+			map[string]string{"name": "常用电话", "link": "http://m.tool.finded.net/index.php?m=Baike&c=Contacts&a=index", "icon": "/static/icons/tel.png"},
+			map[string]string{"name": "参与开发", "link": "dev", "icon": "/static/icons/dev.png"},
 		},
 		"gzhu.ads": []map[string]string{
 			map[string]string{"name": "如何正确使用哪有?", "link": "#", "img": "http://m.nayou.finded.net/uploads/ads/529de50d95a28442dd5718532ba432835c144588.jpg"},
@@ -40,7 +40,31 @@ var (
 			map[string]string{"name": "关于我们", "link": "#"},
 		},
 		// 中大
-		// ...
+		"sysu.name":       "中山大学",
+		"sysu.name.short": "中大",
+		"sysu.icons.mainly": []map[string]string{
+			map[string]string{"name": "成绩", "link": "http://wjw.sysu.edu.cn/", "icon": "/static/icons/score.png"},
+			map[string]string{"name": "课表", "link": "http://wjw.sysu.edu.cn/", "icon": "/static/icons/schedule.png"},
+			map[string]string{"name": "校历", "link": "https://mp.weixin.qq.com/s/BekgSO8SpFJvjv0LuDZoJQ", "icon": "/static/icons/calendar.png"},
+			map[string]string{"name": "个人中心", "link": "[empty]", "icon": "/static/icons/ucenter.png"},
+			map[string]string{"name": "四六级", "link": "http://m.tool.finded.net/index.php?m=Cet&school=sysu", "icon": "/static/icons/cet.png"},
+			map[string]string{"name": "实时公交", "link": "http://m.tool.finded.net/index.php?m=Bus&school=sysu", "icon": "/static/icons/bus.png"},
+			map[string]string{"name": "教学评价", "link": "[empty]", "icon": "/static/icons/teacher.png"},
+			map[string]string{"name": "图书", "link": "http://m.book.finded.net/?code=n1-1xbIFlI7rZNcsKYqhqw5g-2x-2x", "icon": "/static/icons/books.png"},
+		},
+		"sysu.icons.others": []map[string]string{
+			map[string]string{"name": "公选排行榜", "link": "http://www.courstack.com/course/sysu", "icon": "/static/icons/tel.png"},
+			map[string]string{"name": "参与开发", "link": "dev", "icon": "/static/icons/dev.png"},
+		},
+		"sysu.ads": []map[string]string{
+			map[string]string{"name": "如何正确使用哪有?", "link": "#", "img": "http://m.nayou.finded.net/uploads/ads/529de50d95a28442dd5718532ba432835c144588.jpg"},
+		},
+		"sysu.links": []map[string]string{
+			map[string]string{"name": "哪有", "link": "	http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MzA5NDQ1MTkyNA==#wechat_webview_type=1&wechat_redirect"},
+			map[string]string{"name": "有独", "link": "http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MzA5NTg5NzExMg==#wechat_webview_type=1&wechat_redirect"},
+			map[string]string{"name": "种草时间", "link": "http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=MzI3NTA4MDQzMw==#wechat_webview_type=1&wechat_redirect"},
+			map[string]string{"name": "关于我们", "link": "#"},
+		},
 	}
 )
 
