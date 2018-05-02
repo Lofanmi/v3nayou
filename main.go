49d3072069730d6267ae40d6249b9ffcd87c2c12
linhaotong <852226732@qq.com>
2018-05-02 20:27:38 +0800

    main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Lofanmi/v3nayou/cfg"
+	"github.com/Lofanmi/v3nayou/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.DisableConsoleColor()
+
+	cfg.LoadEnv()
+	cfg.InitTmpl()
+	cfg.InitDB()
+
+	r := routes.SetupRouter()
+
+	r.Run("0.0.0.0:5666")
+}
